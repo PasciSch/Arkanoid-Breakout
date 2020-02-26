65812d9e3a6975069d011642af0300d7e1089697
Pascal <pascalschulthess@hotmail.com>
2020-02-26 13:50:03 +0100

    ball movement

---
+++ Assets/Scripts/GameManger.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManger : MonoBehaviour
+{
+    #region Signleton
+    private static GameManger _instance;
+
+    public static GameManger Instance => _instance;
+
+    private void Awake() {
+        if (_instance != null)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            _instance = this;
+        }
+    }
+
+    #endregion
+
+    public bool IsGameStarted { get; set; }
+}
